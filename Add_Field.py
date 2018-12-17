24915242fb4af4030c9a393bcca8acf361408fdf
CarolCrouse <carolanncrouse@gmail.com>
2018-12-17 15:31:27 -0800

    Update Add_Field.py

--- Add_Field.py
+++ Add_Field.py
@@ -1,2 +1,41 @@
-import os, sys, csvimport arcpy
-from arcpy import env  # WHAT DOES THIS DO?#import xl#import pandas as pd        ########################################## Dec 14, 2018# This code adds a new field to the GDB#########################################ws = "C:\\Working_Files\\Example_Python_Code\\Test_Data_Set.gdb\\"#Create list of geodatabase layerslayerList = ['VRI_test_set_sm']  ################################ ADD FIELD ########   ##########################def addFIELD():   for fc in layerList:	   outFeaturePath = ws + fc	   outLayer = fc + "_layer"	   arcpy.MakeFeatureLayer_management(outFeaturePath, outLayer)	   print "Adding field"	   arcpy.AddField_management(outLayer, "STRUC_CODE", "TEXT", "", "", 5)				addFIELD()                
+import os, sys, csv
+import arcpy
+from arcpy import env  # WHAT DOES THIS DO?
+#import xl
+#import pandas as pd
+    
+    
+#########################################
+# Dec 14, 2018
+# This code adds a new field to the GDB
+#########################################
+
+
+ws = "C:\\Working_Files\\Example_Python_Code\\Test_Data_Set.gdb\\"
+
+#Create list of geodatabase layers
+layerList = ['VRI_test_set_sm']  
+
+##########################
+###### ADD FIELD ########   
+##########################
+
+def addFIELD():
+   for fc in layerList:
+	   outFeaturePath = ws + fc
+	   outLayer = fc + "_layer"
+
+	   arcpy.MakeFeatureLayer_management(outFeaturePath, outLayer)
+
+	   print "Adding field"
+
+	   arcpy.AddField_management(outLayer, "STRUC_CODE", "TEXT", "", "", 5)
+     arcpy.AddField_management(outLayer, "ASPEN_TOT_VOL", "LONG", "", "", "")
+				
+
+addFIELD()
+
+    
+    
+    
+    
